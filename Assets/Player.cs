15e83302cd53a4786db36b4f973bdfae3f7addf6
SeptAlfauzan <alfauzansepta@gmail.com>
2020-12-31 00:59:14 +0700

    add enemy attack and chasing player

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float maxhealth;
+    public float maxhealth = 100;
     float currentHealth;
     Rigidbody rigidbody;
 
@@ -25,12 +25,14 @@ public class Enemy : MonoBehaviour
 
     public void TakeDamaged(int damage){
         animator.SetTrigger("Attacked");
-        //Decrease enemy health
+        //Decrease player health
         currentHealth -= damage;
-        //knock back enemy
+        //knock back player
         rigidbody.AddForce(new Vector3(rigidbody.position.x, 0, rigidbody.position.z));
         rigidbody.AddForce(Vector3.up*1f, ForceMode.Impulse);
         
+
+        Debug.Log(currentHealth);
         
         if(currentHealth <= 0){
             Die();
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    public bool isGameEnded = false;
+    Transform player;
+    private void Start() {
+        player = GameObject.Find("PlayerObject").transform;
+    }
+    public void GameOver(){
+        if (!isGameEnded)
+        {
+           
+        }
+    }
+
+    private void Update() {
+        if (player.position.y <= -3)
+            {
+                Restart();
+            }
+    }
+
+    void Restart(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void GameOverMsg(){
+
+    }
+
+    public bool getIsEnded(){
+        return isGameEnded;
+    } 
+}
